3f028d864cf42509c2ab55847923733a641e816f
Bartosz <boleksy14@gmail.com>
2022-07-30 21:29:15 +0200

    added react router with paths to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SingIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
     <>
-     <h1>My App</h1>
+    <Router>
+     <Routes>
+      <Route path='/' element={<Explore/>} />
+      <Route path='/offers' element={<Offers/>} />
+      <Route path='/profile' element={<SingIn/>} />
+      <Route path='/sign-in' element={<SingIn/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/forgot-password' element={<ForgotPassword/>} />
+     </Routes>
+    </Router>
+    {/*Navbar*/}
     </>
   );
 }
