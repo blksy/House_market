60f898aad70569b72c3cd5eaebee2a002ef5a812
Bartosz <boleksy14@gmail.com>
2022-08-01 00:53:14 +0200

    finished creation of navbar with navigtation

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 function Profile(){
     return(
         <div>
-            <h1>Explore</h1>
+            <h1>Profile</h1>
         </div>
     )
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+function SignUp(){
+    return(
+        <div>
+            <h1>Explore</h1>
+        </div>
+    )
+}
+
+export default SignUp;
