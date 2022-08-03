d42724f96571e2b788f490c1a07431435e078547
Bartosz <boleksy14@gmail.com>
2022-08-04 00:00:17 +0200

    continue with Sign in component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,11 +5,56 @@ import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SingIn(){
     const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+    const {email, password} =  formData;
+
+    const navigate = useNavigate();
+
+    const onChange = () => {}
 
     return(
-        <div>
-            <h1>Explore</h1>
-        </div>
+        <>
+           <div className="pageContainer">
+            <header>
+                <p className="pageHeader">Welcome Back!</p>
+            </header>
+            <main>
+                <form>
+                    <input 
+                      type="email" 
+                      className="emailInput" 
+                      placeholder="email" 
+                      id="email" 
+                      value={email} 
+                      onChange={onChange}
+                    />
+                    <div className="passwordInputDiv">
+                        <input 
+                          type={showPassword ? 'text' : 'password'} 
+                          className='passwordInput'
+                          placeholder="Password"
+                          id="password" 
+                          value={password}
+                          onChange={onChange}
+                          />
+                        <img 
+                          src="{visibilityIcon}" 
+                          alt="show password" 
+                          className="showPassword" 
+                          onClick={() => setShowPassword((prevState)=> !prevState)}/>
+                    </div>
+
+                    <Link to= '/forgot-password'
+                    className="forgotPasswordLink">
+                        Forgot Password
+                    </Link>
+                </form>
+            </main>
+           </div>
+        </>
     )
 }
 
