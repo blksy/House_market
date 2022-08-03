bfe513e2881a185f365d447e9b829d51cd7df029
Bartosz <boleksy14@gmail.com>
2022-08-03 23:47:51 +0200

    started working on SignIn component

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+function ForgotPassword(){
+    return(
+        <div>
+            <h1>Explore</h1>
+        </div>
+    )
+}
+
+export default ForgotPassword;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import {Link, useNavigate} from 'react-router-dom'
+import {ReactComponent as ArroRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SingIn(){
+    const [showPassword, setShowPassword] = useState(false);
+
     return(
         <div>
             <h1>Explore</h1>
