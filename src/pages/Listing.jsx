c4ace633d5a7edc1c32e9fd39cffb3d87ffc9443
blksy <79380937+blksy@users.noreply.github.com>
2022-09-18 19:44:26 +0200

    .

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,3 +1,10 @@
+import { useState, useEffect } from 'react'
+import {Link, useNavigate, useParams} from 'reac-router-dom'
+import {getDoc, doc} from 'firebase/firestore'
+import {getAuth} from 'firebase/auth'
+import {db} from '../firebase.config'
+import Spinner from '../components/spinner'
+import shareIcon from '../assets/svg/shareIcon.svg'
 import React from 'react'
 
 function Listing() {
